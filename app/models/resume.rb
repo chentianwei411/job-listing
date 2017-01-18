be0af788cb8e68b99d11e3bb206b1f5f75606ecf
chentianwei <ctw.3@163.com>
2017-01-18 11:13:30 +0800

    admin can see resume list1

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -3,6 +3,7 @@ class Resume < ApplicationRecord
   belongs_to :job
 
   mount_uploader :attachment, AttachmentUploader
-  
+
   validates :content, presence: true
+  validates :attachment, presence: true
 end
