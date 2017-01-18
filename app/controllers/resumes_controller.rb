c9ad44bf2cd5186ed5de5ce55c084d3eb6308dff
chentianwei <ctw.3@163.com>
2017-01-18 10:32:22 +0800

    step 7 upload resume

---
+++ app/controllers/resumes_controller.rb
@@ -0,0 +1,29 @@
+class ResumesController < ApplicationController
+  before_action :authenticate_user!
+
+  def new
+    @job = Job.find(params[:job_id])
+    @resume = Resume.new
+  end
+
+  def create
+    @job = Job.find(params[:job_id])
+    @resume = Resume.new(resume_params)
+    @resume.user = current_user
+    @resume.job = @job
+
+    if @resume.save
+      flash[:notice] = "成功提交简历"
+      redirect_to job_path(@job)
+    else
+      render :new
+    end
+  end
+
+private
+
+  def resume_params
+    params.require(:resume).permit(:content,:attachment)
+
+  end
+end
